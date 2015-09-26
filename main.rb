4d7a32ec6b8661eed8846d75fa6e5fc8054d6c78
chrisvel <chrisvel@users.noreply.github.com>
2015-09-26 18:21:01 +0300

    removed obsolete if from main

--- main.rb
+++ main.rb
@@ -7,50 +7,48 @@ def header
   puts "~" * 50
 end
 
-if __FILE__ == $0
-  case ARGV[0]
-  when nil
-    puts header
-    puts "ERROR: You didn't choose a file with data!"
-    puts
-  else
-    case File.exist?(ARGV[0])
-    when true
-      solve_my_maze = MazeSolver.new(ARGV[0])
-      result = solve_my_maze.solve_dijkstra
+case ARGV[0]
+when nil
+  puts header
+  puts "ERROR: You didn't choose a file with data!"
+  puts
+else
+  case File.exist?(ARGV[0])
+  when true
+    solve_my_maze = MazeSolver.new(ARGV[0])
+    result = solve_my_maze.solve_dijkstra
 
-      case ARGV[1]
-      when "ptr"
-        puts header
-        # print the table and the 1d numbers generated
-        puts result.inspect
+    case ARGV[1]
+    when "ptr"
+      puts header
+      # print the table and the 1d numbers generated
+      puts result.inspect
 
-      when "pst"
-        puts header
-        # print final stats
-        solve_my_maze.print_stats
+    when "pst"
+      puts header
+      # print final stats
+      solve_my_maze.print_stats
 
-      when "pnf"
-        puts header
-        # print the final node set with distances and previous nodes
-        solve_my_maze.print_nodes_final
+    when "pnf"
+      puts header
+      # print the final node set with distances and previous nodes
+      solve_my_maze.print_nodes_final
 
-      when "ptr"
-        puts header
-        # print the table and the 1d numbers generated
-        solve_my_maze.print_table_reverse
+    when "ptr"
+      puts header
+      # print the table and the 1d numbers generated
+      solve_my_maze.print_table_reverse
 
-      when "out"
-        # returns the result of the solution in array format (almost same as previous)
-        puts result.inspect
+    when "out"
+      # returns the result of the solution in array format (almost same as previous)
+      puts result.inspect
 
-      else
-        puts "ERROR: Wrong arguments"
-        puts
-      end
     else
-      puts "ERROR: File does not exist."
+      puts "ERROR: Wrong arguments"
       puts
     end
+  else
+    puts "ERROR: File #{ARGV[0]} does not exist."
+    puts
   end
 end
